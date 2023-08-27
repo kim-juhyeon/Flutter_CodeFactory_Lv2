f5630d5609b5fb09d7e7e8339afa50abf2d644b3
jju <totkfka4@gmail.com>
2023-08-28 00:03:55 +0900

    bloc Listener

--- state_management_bloc/state_bloc/lib/main.dart
+++ state_management_bloc/state_bloc/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:state_bloc/cubits/cubit/counter_cubit.dart';
+import 'package:state_bloc/other_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,15 +32,36 @@ class MyHomepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocBuilder<CounterCubit, CounterState>(
-        builder: (context, state) {
-          return Center(
-            child: Text(
-              '${state.counter}',
-              style: TextStyle(fontSize: 51.0),
-            ),
-          );
+      body: BlocListener<CounterCubit, CounterState>(
+        listener: (context, state) {
+          if (state.counter == 3) {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  content: Text('counter is ${state.counter}'),
+                );
+              },
+            );
+          } else {
+            if (state.counter == -1) {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return OtherPage();
+              }));
+            }
+          }
         },
+        child: BlocBuilder<CounterCubit, CounterState>(
+          //제너릭에 부여할 함수 class를 기입합니다. 이후 state를 쉽게 접근이 가능함
+          builder: (context, state) {
+            return Center(
+              child: Text(
+                '${state.counter}',
+                style: TextStyle(fontSize: 51.0),
+              ),
+            );
+          },
+        ),
       ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
--- state_management_bloc/state_bloc/lib/other_page.dart
+++ state_management_bloc/state_bloc/lib/other_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({Key? key}) : super(key: key);
+class OtherPage extends StatelessWidget {
+  const OtherPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
